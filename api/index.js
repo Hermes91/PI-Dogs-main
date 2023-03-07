9044bc6dd421d71498f0921f14146169b898151b
Hermes <hermeschacon@live.com.ar>
2023-03-07 15:53:08 -0300

    update .env

--- api/index.js
+++ api/index.js
@@ -20,12 +20,12 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const { getTemp } = require('./src/middleware/middleware')
-
-
+require('dotenv').config()
+const { PORT } = process.env
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, async () => {
-    console.log('woof woof at 3001'); // eslint-disable-line no-console
+  server.listen(PORT, async () => {
+    console.log('woof woof at', PORT); // eslint-disable-line no-console
     await getTemp()
   });
 });
